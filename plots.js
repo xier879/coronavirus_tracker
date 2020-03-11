ab57eb26382aa22d25b31b5b5ed0f24ef8889724
roayahelal <56054235+roayahelal@users.noreply.github.com>
2020-03-11 02:55:00 -0400

    added plotly

--- plots.js
+++ plots.js
@@ -1,26 +1,15 @@
-
-d3.json("../Roaya_H_group_project2/data.json", function(x) {
-    console.log(x);
-  
-    for ( var i = 0; i < x.length; i++) {
-      var Country = x[i].Country;
-      var Cases = +x[i].TotalCases;
-      var Deaths = +x[i].TotalDeaths;
-      var NewCases = +x[i].NewCases;
-      var NewDeaths = +x[i].NewDeaths;
-      var MortRate = (( +x[i].TotalDeaths / +x[i].TotalCases ) * 100).toFixed(2) + '%';
-      var coords = [x[i].Latitude, x[i].Longitude]
-    }}
-var trace1 = {
-        x: Country,
-        y: [Deaths / Cases],
+      d3.json("data.json", function(data) {
+      var data = data;
+      var trace1 = {
+        x: data.map(row => row.Country),
+        y: data.map(row => row.TotalDeaths/row.TotalCases),
         type: "bar"
       };
-      
-      var data = [trace1];
+      var plotData = [trace1];
       
       var layout = {
-        title: "'Bar' Chart"
+        title: "Mortality Rate"
       };
       
-      Plotly.newPlot("plot", data, layout);
+      Plotly.newPlot("plot", plotData, layout)});
+      
